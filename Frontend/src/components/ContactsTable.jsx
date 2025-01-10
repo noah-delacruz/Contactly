ce8337bfacc8b91698f8ae214bedf1d87664a753
Noah dela Cruz <delacruznoah12898@yahoo.com>
2025-01-09 18:15:49 -0800

    Make rows editable

--- Frontend/src/components/ContactsTable.jsx
+++ Frontend/src/components/ContactsTable.jsx
@@ -2,6 +2,14 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { DataGrid } from "@mui/x-data-grid";
 import Paper from "@mui/material/Paper";
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+} from "@mui/material";
 
 export default function ContactsTable() {
     const [contacts, setContacts] = useState([]);
@@ -16,21 +24,25 @@ export default function ContactsTable() {
             field: "first_name",
             headerName: "First Name",
             width: 130,
+            editable: true,
         },
         {
             field: "last_name",
             headerName: "Last Name",
             width: 130,
+            editable: true,
         },
         {
             field: "zip_code",
             headerName: "Zip Code",
             width: 130,
+            editable: true,
         },
         {
             field: "date_of_birth",
             headerName: "Date of Birth",
             width: 130,
+            editable: true,
         },
     ];
 
@@ -55,19 +67,93 @@ export default function ContactsTable() {
         console.log("Contacts updated:", contacts);
     }, [contacts]);
 
+    const handleRowClick = (e) => {
+        console.log(e);
+    };
+
+    const handleClick = (e) => {
+        console.log("clicked");
+    };
+
     return (
         <>
             <h1>Contactly</h1>
             <Paper sx={{ width: "100%" }}>
                 <DataGrid
+                    editMode="row"
                     rows={contacts}
                     columns={columns}
                     initialState={{ pagination: { paginationModel } }}
                     pageSizeOptions={[100]}
-                    checkboxSelection
+                    // checkboxSelection
+                    onCellClick={handleRowClick}
                     sx={{ border: 0 }}
                 />
             </Paper>
+            {/* <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>ID</TableCell>
+                            <TableCell align="right">First Name</TableCell>
+                            <TableCell align="right">Last Name</TableCell>
+                            <TableCell align="right">Zip Code</TableCell>
+                            <TableCell align="right">Date of Birth</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {contacts.map((contact) => (
+                            <TableRow
+                                key={contact.id}
+                                sx={{
+                                    "&:last-child td, &:last-child th": {
+                                        border: 0,
+                                    },
+                                }}
+                            >
+                                <TableCell component="th" scope="row">
+                                    {contact.id}
+                                </TableCell>
+                                <TableCell align="right" onClick={handleClick}>
+                                    {contact.first_name}
+                                </TableCell>
+                                <TableCell align="right">
+                                    {contact.last_name}
+                                </TableCell>
+                                <TableCell align="right">
+                                    {contact.zip_code}
+                                </TableCell>
+                                <TableCell align="right">
+                                    {contact.date_of_birth}
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer> */}
+
+            {/* <table border="1">
+                <thead>
+                    <tr>
+                        <th onClick={handleClick}>ID</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Zip Code</th>
+                        <th>Date of Birth</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {contacts.map((contact) => (
+                        <tr key={contact.id}>
+                            <td>{contact.id}</td>
+                            <td>{contact.first_name}</td>
+                            <td>{contact.last_name}</td>
+                            <td>{contact.zip_code}</td>
+                            <td>{contact.date_of_birth}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table> */}
         </>
     );
 }
